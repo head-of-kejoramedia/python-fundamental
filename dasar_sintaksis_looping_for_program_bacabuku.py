e060d05753061361a3b33c5442a7fa2e0bb0f5d6
hasan <kejoramedia@gmail.com>
2023-01-23 18:48:54 +0700

    praktek sequencing dan branching

---
+++ dasar_sintaksis_looping_for_program_bacabuku.py
@@ -0,0 +1,16 @@
+"""
+Program looping membaca buku
+"""
+
+jumlah_buku = 10
+print('Ibu menyuruh,"Baca semua buku"')
+
+jumlah_buku_dibaca = 0
+print (f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
+
+for jumlah_buku_dibaca in range (1,jumlah_buku+1):
+    print(f'Buku ke {jumlah_buku_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}')
+
+
