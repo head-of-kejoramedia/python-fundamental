c4139f7499d6f9d2eb6d055820d383e81912a925
hasan <kejoramedia@gmail.com>
2023-01-23 19:03:59 +0700

    praktek looping while program baca buku

---
+++ dasar_sintaksis_looping_while_program_bacabuku.py
@@ -0,0 +1,14 @@
+"""
+Looping - while, Program baca buku
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+
+jumlah_buku_dibaca = 0
+
+while jumlah_buku_dibaca < jumlah_buku:
+    jumlah_buku_dibaca = jumlah_buku_dibaca+1
+    print(f'Buku ke {jumlah_buku_dibaca} sudah dibaca')
+
+print(f'Jumlah buku sudah dibaca {jumlah_buku_dibaca}')
