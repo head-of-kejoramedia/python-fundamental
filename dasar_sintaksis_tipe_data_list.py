7bf3570038df60b99f117ee62225bd09e9d6887e
hasan <kejoramedia@gmail.com>
2023-01-25 06:22:28 +0700

    dasar sintaksi tipe data list

---
+++ dasar_sintaksis_tipe_data_list.py
@@ -0,0 +1,10 @@
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Thing First']
+print('Tampilkan Variable')
+print (daftar_buku)
+
+for buku in daftar_buku:
+    print(buku)
+
+print ('Indeks ke dua dari daftar buku')
+
+print(daftar_buku[1])
