38d8c98c4aa3cdd3efab5f5f519c153ab7867bd3
hasan <kejoramedia@gmail.com>
2023-01-26 06:17:28 +0700

    dasar sintaksi tipe data list

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -2,9 +2,23 @@ daftar_buku = ['Seven Habits', 'How to Influence People', 'First Thing First']
 print('Tampilkan Variable')
 print (daftar_buku)
 
+print('\nProses semua dengan for in')
 for buku in daftar_buku:
     print(buku)
 
-print ('Indeks ke dua dari daftar buku')
+print ('\nTampilkan indeks tertentu dari daftar buku')
+print(daftar_buku[0])
+print(daftar_buku[2])
 
-print(daftar_buku[1])
+print('\nTampilkan dengan for in range')
+daftar_buku = ['Seven Habits', 'How to Influence People', 'First Thing First','4DX',101]
+print(daftar_buku[0:2])
+
+for i in range(0,len(daftar_buku)):
+    print (i)
+
+print('\n')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+print('\nTambahkan 1 Buku Baru')
+print(daftar_buku.append('Dunia Matematika'))
