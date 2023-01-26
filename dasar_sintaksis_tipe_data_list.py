02931b2ac80a69fe0d76510a8ffa99dc81925747
hasan <kejoramedia@gmail.com>
2023-01-27 00:07:52 +0700

    dasar sintaksi tipe data list

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -28,3 +28,25 @@ for i in daftar_buku:
 print('\n4#List Python Bisa Campur (Integer - String) dan Dinamis (Bisa Didefinisikan Ulang)')
 daftar_buku = [12,'Kenji Volume 3',-313,3.14]
 print(daftar_buku)
+
+print('\n5# Menghapus List')
+daftar_buku.clear()
+print(daftar_buku)
+for i in daftar_buku:
+    print(i)
+
+print('\n6# Mengganti Elemen List')
+daftar_buku = ['Seven Habits','How to Influence People','First Thing First', '4DX','Dunia Matematika']
+daftar_buku[0] = 'Eight Habits'
+print(daftar_buku)
+
+print('\n7# Mengambil dan Menaruh di Variable Baru Elemen List Menggunakan Pop')
+buku = daftar_buku.pop(0)
+print(buku)
+print(daftar_buku)
+
+daftar_buku = ['Seven Habits','How to Influence People','First Thing First', '4DX','Dunia Matematika']
+print(daftar_buku.pop())
+
+daftar_buku = ['Seven Habits','How to Influence People','First Thing First', '4DX','Dunia Matematika']
+print(daftar_buku.pop(-2))
