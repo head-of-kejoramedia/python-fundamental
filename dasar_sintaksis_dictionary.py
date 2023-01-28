3e2c4ea166ffd9e92bddf113f2ddcd26fea63cb3
hasan <kejoramedia@gmail.com>
2023-01-28 08:45:19 +0700

    dasar sintaksi tipe data list

---
+++ dasar_sintaksis_dictionary.py
@@ -0,0 +1,15 @@
+users = {"id": 1,
+"name": "Leanne Graham",
+"username": "Bret",
+"email": "Sincere@april.biz"}
+
+print(users)
+print(users["email"])
+
+print('\n')
+
+print(users)
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
